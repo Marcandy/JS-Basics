5de43f6127c51bc62540064178c814c851f29784
marcandy <marcandy.nj@gmail.com>
2016-12-02 16:00:23 -0700

    review

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -31,7 +31,9 @@ var name = 'Tyler';
 //then alerts "Welcome, " plus whatever the users name is.
 
   //Code Here
-
+  function welcome() {
+    alert("Welcome, " + getName());
+  }
 //////////////////PROBLEM 4////////////////////
 
 
